aa2db9f1477b259c66a2fe416c7f66abbafe3567
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-28 12:42:11 -0400

    using secondary

--- lib/screens/settings/appearance.dart
+++ lib/screens/settings/appearance.dart
@@ -12,9 +12,9 @@ import 'package:open_ui/open_ui.dart';
 
 class AppearanceSettingsScreen extends StatelessWidget {
   final int? target;
-  final bool? primary;
+  final bool? secondary;
 
-  const AppearanceSettingsScreen({super.key, this.target, this.primary});
+  const AppearanceSettingsScreen({super.key, this.target, this.secondary});
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +55,7 @@ class AppearanceSettingsScreen extends StatelessWidget {
                 title: config.ezL10n.gColor,
                 icon: EzIcon(config, Icons.palette, semanticLabel: config.ezL10n.gColor),
                 subSettings: <EzSubSetting>[EzSubSetting.qckColor, EzSubSetting.advColor],
-                fromStorage: () => EzCM.get(advancedColorsKey) == true
+                fromStorage: () => (secondary ?? EzCM.get(advancedColorsKey) == true)
                     ? EzSubSetting.advColor
                     : EzSubSetting.qckColor,
                 build: (EzSubSetting subSec) => EzColorSettings(
@@ -72,8 +72,9 @@ class AppearanceSettingsScreen extends StatelessWidget {
                 title: config.ezL10n.gDesign,
                 icon: EzIcon(config, Icons.design_services, semanticLabel: config.ezL10n.gDesign),
                 subSettings: <EzSubSetting>[EzSubSetting.butDesign, EzSubSetting.pagDesign],
-                fromStorage: () =>
-                    EzCM.get(pageTabKey) == true ? EzSubSetting.pagDesign : EzSubSetting.butDesign,
+                fromStorage: () => (secondary ?? EzCM.get(pageTabKey) == true)
+                    ? EzSubSetting.pagDesign
+                    : EzSubSetting.butDesign,
                 build: (EzSubSetting subSec) => EzDesignSettings(
                   config,
                   target: subSec,
@@ -87,8 +88,9 @@ class AppearanceSettingsScreen extends StatelessWidget {
                 title: config.ezL10n.gText,
                 icon: EzIcon(config, Icons.text_format, semanticLabel: config.ezL10n.gText),
                 subSettings: <EzSubSetting>[EzSubSetting.qckText, EzSubSetting.advText],
-                fromStorage: () =>
-                    EzCM.get(advancedTextKey) == true ? EzSubSetting.advText : EzSubSetting.qckText,
+                fromStorage: () => (secondary ?? EzCM.get(advancedTextKey) == true)
+                    ? EzSubSetting.advText
+                    : EzSubSetting.qckText,
                 build: (EzSubSetting subSec) => EzTextSettings(config, target: subSec),
               ),
             ],
