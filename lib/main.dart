c98fdf26b62afdc546aad3b4d24fc048c79ffe52
empathictech <105748722+empathictech@users.noreply.github.com>
2026-08-29 22:33:05 -0400

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ import './utils/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:open_ui/open_ui.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:open_ui/open_ui.dart';
 
 void main() async {
   // Configure the app //
